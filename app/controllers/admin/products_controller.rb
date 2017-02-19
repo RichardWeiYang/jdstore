5905deae7f49bfbfc0cb2b5d182e57f708e58738
Wei Yang <richard.weiyang@gmail.com>
2017-02-19 14:55:36 +0800

    basic admin/product CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,54 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_param)
+      flash[:notice] = "Product information updated"
+      redirect_to admin_products_path
+    else
+      flash[:warning] = "can't update product information"
+      render :edit
+    end
+  end
+
+  def create
+    @product = Product.new(product_param)
+
+    if @product.save
+      flash[:notice] = "Product information created"
+      redirect_to admin_products_path
+    else
+      flash[:warning] = "can't create a new product"
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:warning] = "product deleted"
+    redirect_to admin_products_path
+  end
+
+  private
+
+  def product_param
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
+end
