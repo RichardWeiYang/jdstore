5c55d3f873c077b451f6a07ddd7131041d65a2cf
Wei Yang <richard.weiyang@gmail.com>
2017-02-20 16:35:36 +0800

    order: add pay methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,6 +33,22 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay_with_alipay
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("alipay")
+    @order.pay!
+
+    redirect_to order_path(@order.token), notice: "Paied with Alipay"
+  end
+
+  def pay_with_wechat
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("wechat")
+    @order.pay!
+
+    redirect_to order_path(@order.token), notice: "Paied with Wechat"
+  end
+
   private
 
   def order_params
