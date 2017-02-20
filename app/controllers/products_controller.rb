19f351f6156e88f2f64e7720ea9d87b63eb11093
Wei Yang <richard.weiyang@gmail.com>
2017-02-20 09:57:58 +0800

    cart: one item of each kind product in cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,12 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "Added to cart"
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "Added \'#{@product.title}\' to cart"
+    else
+      flash[:warning] = "You have already had \'#{@product.title}\' in cart"
+    end
     redirect_to :back
   end
 end
