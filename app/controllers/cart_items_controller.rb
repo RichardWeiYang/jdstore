7dce442c4c410eabaa7637a1c97175d7e15f1e14
Wei Yang <richard.weiyang@gmail.com>
2017-02-20 10:21:21 +0800

    cart_item: cart_item quantity can't exceed stock

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,7 +14,13 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
+
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "Quantity of \'#{@cart_item.product.title}\' updated to #{@cart_item.quantity}"
+    else
+      flash[:warning] = "Only #{@cart_item.product.quantity} \'#{@cart_item.product.title}\' left"
+    end
     redirect_to carts_path
   end
 
