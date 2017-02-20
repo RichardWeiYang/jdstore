3d052e30941b16db79e400d00464f8749a352f87
Wei Yang <richard.weiyang@gmail.com>
2017-02-20 09:42:45 +0800

    cart_item: implement delete control

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,13 @@
+class CartItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def destroy
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
+    @cart_item.destroy
+
+    flash[:notice] = "Removed #{@product.title} from your cart"
+    redirect_to :back
+  end
+end
