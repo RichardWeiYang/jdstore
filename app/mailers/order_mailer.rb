b78f1f4939fc8c7996e79ee60ef405b270cf73f4
Wei Yang <richard.weiyang@gmail.com>
2017-02-21 11:00:01 +0800

    admin/order_control: send mail when cancel/deliver order

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -14,4 +14,20 @@ class OrderMailer < ApplicationMailer
 
     mail(to: "service@jdstore.com", subject: "[JDstore] #{@user.nickname} cancel order #{@order.token}")
   end
+
+  def notify_ship(order)
+    @order = order
+    @user  = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "[JDstore] Your Order #{order.token} is delivering.")
+  end
+
+  def notify_cancel(order)
+    @order = order
+    @user  = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "[JDstore] Your Order #{order.token} is cancelled.")
+  end
 end
