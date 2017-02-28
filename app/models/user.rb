e4b8d9f1c1d01cccbcc02b2ccdc4ca1902959a3c
Wei Yang <richard.weiyang@gmail.com>
2017-02-28 16:34:52 +0800

    user: add a field of paid_orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :orders
+
+  has_many :paid_orders, -> { where(:is_paid => true) }, :class_name => "Order", :foreign_key => "user_id"
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
