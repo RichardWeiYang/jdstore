5ed14a7e7f92918599ca06d27b7eed051b44df70
Wei Yang <richard.weiyang@gmail.com>
2017-03-09 17:56:21 +0800

    refactor: public product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :add_to_cart]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def add_to_cart
-    @product = Product.find(params[:id])
     if !current_cart.products.include?(@product)
       current_cart.add_product_to_cart(@product)
       flash[:notice] = "Added \'#{@product.title}\' to cart"
@@ -16,5 +16,11 @@ class ProductsController < ApplicationController
       flash[:warning] = "You have already had \'#{@product.title}\' in cart"
     end
     redirect_to :back
+  end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
