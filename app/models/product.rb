84a74a98502e4a923071193e318f994aa65f5756
Wei Yang <richard.weiyang@gmail.com>
2017-03-21 15:29:28 +0800

    product associate with group

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :groupships
+  has_many :groups, :through => :groupships
   mount_uploader :image, ImageUploader
   acts_as_list
 end
